93830277cb43a50834891969897d832c7b5c9d65
Ejleone <ejleone@localhost.localdomain>
2019-06-29 17:48:11 -0400

    initiale setup

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-
 import express from 'express';
+import {foremen ,Orders } from './db';
+
+
 const app = express();
 
-app.post('/api/v1/foreman', (req, res) => {
+app.post('/', (req, res) => {
+  console.log("Stk Request Found here");
+    console.log(req.params);
+  console.log(req.body);
 
+
+  let sessionId = req.body.sessionId;
+  let serviceCode = req.body.serviceCode;
+  let phoneNumber = req.body.phoneNumber;
+  let text  = req.body.text;
+
+  var length = text.split("*").length
+  var txt = req.body.split("*")
+
+  if(text === ""){
+      message  = "Welcome to Inuua Diburse "
+  }
+  else if ( text === "1" ){
+    message = "Choose which item you want to Disburse"
+  }
+  else if ( text === "2"){
+
+  }
+  else if (text === "3"){
+
+  }
+  else {
+     message = "END = Wrong input"
+  }
+  res.send(message,200)
 })
 
 const PORT = 5000;
@@ -11,4 +41,3 @@ const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
 });
-
