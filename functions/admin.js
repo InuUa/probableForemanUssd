4bd70d113ddac6e668bb500829d7e8621be2a180
Ej <Ejgithinji@gmail.com>
2019-07-26 05:08:18 +0300

    implemente the apis

--- functions/admin.js
+++ functions/admin.js
@@ -11,9 +11,22 @@ const foremen = require('./db')
 const Orders = require('./db')
 //import {foremen ,Orders } from './db';
 
+let db = admin.firestore();
+stockRef = db.collection('Stock')
+TransactionRef = db.collection('Transaction')
+
 const app = express();
 app.use(cookieParser)
 
+const CreateTransaction = async (Transaction) => {
+  TransactionRef.doc().set(Transaction)
+    .then((pr) => {
+      res.status(200).send({ error: false })
+    })
+    .catch((err) => {
+      res.status(500).send({ error: true, message: err })
+    })
+}
 
 
 app.post('/', (req, res) => {
@@ -22,7 +35,18 @@ app.post('/', (req, res) => {
   }
   else {
     //Save Stock to Db
-
+    stockRef.doc().set({
+      name: req.body.name,
+      amount: req.body.amount,
+      itype: req.body.itype,
+      measurements: req.body.measurements
+    })
+      .then((pr) => {
+        res.status(200).send({ error: false })
+      })
+      .catch((err) => {
+        res.status(500).send({ error: true, message: err })
+      })
   }
 })
 
@@ -31,9 +55,20 @@ app.post("/transaction", () => {
     res.status(422).send({ error: true, message: "Incomplete information" })
   }
   else {
+    let trans = { product: req.body.productid, amount: req.body.amount, name: req.body.name, time: req.body.time, labourer: req.body.labourer }
+    if (await CreateTransaction(trans)) {
+      //Transaction 
+      DecreaseBy = firebase.firestore.FieldValue.increment(-req.body.amount);
+      db.collection('Stock').doc(req.body.productid)
+        .update({
+          amount: DecreaseBy
+        })
+    }
+
 
   }
   //Add Transaction 
+
   //if Succesful update Stock
 
 })
@@ -43,13 +78,34 @@ app.post("/transaction", () => {
 
 app.get('/', (req, res) => {
   //Retreive the Stock and current amounnt
-  res.status(200).send({ error: false, message: [] })
+  stockRef.get()
+    .then((snapshot) => {
+      //snapshot.forEach((doc) => {
+      // console.log(doc.id, '=>', doc.data());
+      // });
+      res.status(200).send({ error: false, message: snapshot })
+    })
+    .catch((err) => {
+      console.log('Error getting documents', err);
+      res.status(500).send({ error: true, message: err })
+    });
+
 })
 
 app.get('/transaction', (req, res) => {
   //Retreive  All transaction 
-
-  res.status(200).send({ error: false, message: [] })
+  TransactionRef.get()
+    .then((snapshot) => {
+      //snapshot.forEach((doc) => {
+      // console.log(doc.id, '=>', doc.data());
+      // });
+      res.status(200).send({ error: false, message: snapshot })
+    })
+    .catch((err) => {
+      console.log('Error getting documents', err);
+      res.status(500).send({ error: true, message: err })
+    });
+  //res.status(200).send({ error: false, message: [] })
 })
 
 
